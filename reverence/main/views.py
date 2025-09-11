23c5cb6885ea2813d2736216a10cc33725ce0fcd
Boris <bobleo@yandex.ru>
2025-09-11 12:05:21 +0300

    4st 110925 1206 commit

--- reverence/main/views.py
+++ reverence/main/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView  
+from .models import ClothingItem, Category, Size
+from django.db.models import Q
 
 
+class CataloglogViem(ListView):
+    model = ClothingItem
+    template_names = 'main/product/list.html'
+    context_object_name = 'clothing_items'
+
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_slugs = self.request.GET.getlist('category')
+        size_names = self.request.GET.getlist('size')
+        min_price = self.request.GET.getlist('min_price')
+        max_price = self.request.GET.getlist('max_price')
+
+        if category_slugs:
+            queryset = queryset.filter(category__slug__in=category_slugs)
+       
+        if size_names:
+            queryset = queryset.filter(
+                Q(sizes__name__in=category_names) & Q(sizes__clothingitemsize__available=True)
+            )
+
+        if min_price:
+            queryset = queryset.filter(price__gte=min_price)
+
+        if max_price:
+            queryset = queryset.filter(price__lte=max_price)    
+
+        return queryset
+
+    def  def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['sizes'] = Size.objects.all()
+        context['selected_categories'] = self.request.GET.getlist('category')
+        context['selected_sizes'] = self.request.GET.getlist('size')
+        context['min_price'] = self.request.GET.getlist('min_price', '')
+        context['max_price'] = self.request.GET.getlist('max_price', '')
+        
+        return context
+
+
+    
+        
+    
